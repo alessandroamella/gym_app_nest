11b104d6d55af5c3c705a03aef0db1c695303c07
Alessandro Amella <info@bitrey.it>
2024-11-30 12:51:05 +0100

    Add sample msg fn

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -1,5 +1,6 @@
 import { Inject, Module } from '@nestjs/common';
-import admin from 'firebase-admin';
+import firebaseAdmin from 'firebase-admin';
+import type { Notification } from 'firebase-admin/lib/messaging/messaging-api';
 import { readFile } from 'fs/promises';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { join } from 'path';
@@ -20,9 +21,20 @@ export class FirebaseModule {
       join(cwd(), process.env.FIREBASE_SERVICE_ACCOUNT_PATH),
       'utf8',
     );
-    const app = admin.initializeApp({
-      credential: admin.credential.cert(JSON.parse(serviceAccount)),
+    const app = firebaseAdmin.initializeApp({
+      credential: firebaseAdmin.credential.cert(JSON.parse(serviceAccount)),
     });
     this.logger.info(`Firebase app ${app.name} initialized`);
+  }
+
+  public async sendMessage(token: string, notification: Notification) {
+    this.logger.debug('Sending message to Firebase');
+    await firebaseAdmin.messaging().send({
+      condition: "'all'",
+      token,
+      topic: 'all',
+      notification,
+    });
+    this.logger.info('Message sent to Firebase');
   }
 }
